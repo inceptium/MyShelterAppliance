178ac9e93ab6c7504dfca2c40e5190b8e83af83f
Antonio <cpmatrix@mac.com>
2021-10-22 11:41:33 +0200

    Update

--- start_myshelter_service.sh
+++ start_myshelter_service.sh
@@ -14,7 +14,7 @@ while [ $INCEPTIUM_ACTIVE -eq 1 ];
 do
     cp /root/BaseDati/temp/LCimp_new.rsd /root/BaseDati/LCimp.rsd
     #java -Xms128m -Xmx256m  -XX:+UseLinuxPosixThreadCPUClocks -agentpath:/root/profiling/lib/deployed/jdk16/linux-amd64/libprofilerinterface.so=/root/profiling/lib,5140  -cp "InceptiumServices.jar:/root/InceptiumLinuxAppliance/InceptiumServices/lib/*:/root/IncStorage/InceptiumAppStore/*" inceptiumserver.core.server.IncServer noswing updatedatabase
-        java -Xms128m -Xmx512m -cp "myshelter.jar:/root/MyShelterAppliance/MyShelter/lib/*" myshelter.Main noswing $MEDIAPATH $BASEDATI /root/MyShelterAppliance/MyShelter/CustomLib eth0 >> $LOGFILE 2>&1
+        java -Xms128m -Xmx512m -cp "myshelter.jar:/root/MyShelterAppliance/MyShelter/lib/*" myshelter.Main noswing $MEDIAPATH $BASEDATI /root/MyShelterAppliance/MyShelter/CustomLib eth0 eth1 >> $LOGFILE 2>&1
         MSG=$?
         echo "0" > /root/BaseDati/etc/vpn/led/led2
         echo "Messaggio ricevuto: $MSG" >>  $LOGFILE
