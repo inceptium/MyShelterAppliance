c84c8e29e23ec7c314c53cea5d269056ce56165d
Antonio <cpmatrix@mac.com>
2021-10-29 19:58:16 +0200

    Update

--- start_appliance_fix.sh
+++ start_appliance_fix.sh
@@ -2,4 +2,4 @@
 systemctl disable inceptiumdiscovery.service
 systemctl disable myshelter.service
 systemctl disable inceptiumservice.service
-cp /root/MyShelterAppliance/reset_head_from_git /root/reset_head_from_git.sh
+cp /root/MyShelterAppliance/reset_head_from_git.sh /root
