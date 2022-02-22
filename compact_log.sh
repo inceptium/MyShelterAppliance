816cfee82874750db7891989901cb219e1b89edf
Antonio <cpmatrix@mac.com>
2022-02-22 12:09:02 +0100

    Update Discover

--- compact_log.sh
+++ compact_log.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-tail -n10 /root/BaseDati/myshelter_srv.log > /root/BaseDati/myshelter_srv2.log
+tail -n1000 /root/BaseDati/myshelter_srv.log > /root/BaseDati/myshelter_srv2.log
 cat /root/BaseDati/myshelter_srv2.log > /root/BaseDati/myshelter_srv.log
+rm /root/BaseDati/myshelter_srv2.log
