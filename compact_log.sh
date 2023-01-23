b7a19a939f619419e59154c4dceef1c4066f2b64
Antonio <cpmatrix@mac.com>
2023-01-23 18:33:12 +0100

    Update

--- compact_log.sh
+++ compact_log.sh
@@ -2,3 +2,6 @@
 tail -n1000 /root/BaseDati/myshelter_srv.log > /root/BaseDati/myshelter_srv2.log
 cat /root/BaseDati/myshelter_srv2.log > /root/BaseDati/myshelter_srv.log
 rm /root/BaseDati/myshelter_srv2.log
+tail -n1000 /root/IncStorage/log/inceptium_srv.log > /root/IncStorage/log/inceptium_srv2.log
+cat /root/IncStorage/log/inceptium_srv2.log > /root/IncStorage/log/inceptium_srv.log
+rm /root/IncStorage/log/inceptium_srv2.log
